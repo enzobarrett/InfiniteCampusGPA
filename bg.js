f2218cf8e14b1d4bfabb6652dbaea39a8fe717b9
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-15 08:48:45 -0700

    add dom manipulation

--- bg.js
+++ bg.js
@@ -2,5 +2,11 @@ var gpa;
 chrome.runtime.onMessage.addListener(function(message) {
     if (message.myVar) {
         gpa = message.myVar;
+        var views = chrome.extension.getViews({
+        type: "popup"
+        });
+        for (var i = 0; i < views.length; i++) {
+        views[i].document.getElementById('x').innerHTML = gpa;
+      }
     }
 });
