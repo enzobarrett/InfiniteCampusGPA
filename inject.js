083f85ee0b3ac745104cf3e2e123a2fbcbbafbb3
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-14 20:40:16 -0700

    Update inject.js

---
+++ inject.js
@@ -0,0 +1,25 @@
+  function toGPA(x) {
+    if (x == 'A') {
+      return 4;
+    }
+    if (x == 'B') {
+      return 3;
+    }
+    if (x == 'C') {
+      return 2;
+    }
+    if (x == 'D') {
+      return 1;
+    }
+    if (x == 'F') {
+      return 0;
+    }
+  }
+  var a;
+  var sum = 0;
+  var letters = document.querySelectorAll('.inProgressGrade b');
+  for (i = 0; i < letters.length; i++) {
+    a = toGPA(letters[i].textContent);
+    sum = sum + a;
+  }
+  console.log('Calculated GPA is = ' + sum / i + " SOLID!!!");
