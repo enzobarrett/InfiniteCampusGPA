02cc2228853a3d8be98dbe07dba1f1a64ad960e4
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-15 08:33:15 -0700

    add var gpa

--- bg.js
+++ bg.js
@@ -1,8 +1,6 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
+var gpa;
+chrome.runtime.onMessage.addListener(function(message) {
+    if (message.myVar) {
+        gpa = message.myVar;
+    }
+});
