39f61d6229fd0a3edb16737ce64415ae9de957bb
Enzo Barrett <elbarrett01@bvsd.org>
2017-12-15 11:50:18 -0700

    make work with ic

--- inject.js
+++ inject.js
@@ -1,3 +1,4 @@
+window.onload = function() {
   function toGPA(x) {
     if (x == 'A') {
       return 4;
@@ -15,14 +16,42 @@
       return 0;
     }
   }
-  var a;
-  var sum = 0;
+  //  var a;
+  //  var sum = 0;
   var letters = document.querySelectorAll('.inProgressGrade b');
-  for (i = 0; i < letters.length; i++) {
-    a = toGPA(letters[i].textContent);
-    sum = sum + a;
-  }
-  console.log('Calculated GPA is = ' + sum / i + " SOLID!!!");
-  var gpa = sum/i;
+  //for (i = 0; i < letters.length; i++) {
+  var i = 0;
+  console.log(letters[i].textContent);
+  //  }
+  //  console.log(sum);
+  /*function toGPA(x) {
+    if (x == 'A') {
+      return 4;
+    }
+    if (x == 'B') {
+      return 3;
+    }
+    if (x == 'C') {
+      return 2;
+    }
+    if (x == 'D') {
+      return 1;
+    }
+    if (x == 'F') {
+      return 0;
+    }
+  };
+  var x = toGPA("A");
+  console.log(x);
+  //var sum = 0;
+  var letters = document.querySelectorAll('.gradesBody .inProgressGrade b').textContent;
+  console.log(letters[0].textContent);
 
-  chrome.runtime.sendMessage({myVar: gpa});
+  for (i = 0; i < letters.length; i++) {
+    var number = letters[i].textContent;
+    console.log(number);
+  }
+
+  //chrome.runtime.sendMessage({myVar: gpa});
+  */
+}
