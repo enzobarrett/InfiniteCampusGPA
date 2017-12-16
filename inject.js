cb8f2ec10aede81c31b85de88f3dce0488a97435
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-15 18:21:00 -0700

    clean up code

--- inject.js
+++ inject.js
@@ -16,45 +16,18 @@ window.onload = function() {
       return 0;
     }
   }
-  //  var a;
+  
   driver.switch_to_default_content()
   frame = driver.find_element_by_name('frameDetail')
   
-  //  var sum = 0;
   var letters = document.frame.querySelectorAll('.inProgressGrade b');
-  //for (i = 0; i < letters.length; i++) {
-  var i = 0;
-  console.log(letters[i].textContent);
-  //  }
-  //  console.log(sum);
-  /*function toGPA(x) {
-    if (x == 'A') {
-      return 4;
-    }
-    if (x == 'B') {
-      return 3;
-    }
-    if (x == 'C') {
-      return 2;
-    }
-    if (x == 'D') {
-      return 1;
-    }
-    if (x == 'F') {
-      return 0;
-    }
-  };
-  var x = toGPA("A");
-  console.log(x);
-  //var sum = 0;
-  var letters = document.querySelectorAll('.gradesBody .inProgressGrade b').textContent;
-  console.log(letters[0].textContent);
-
+  var sum;
+  
   for (i = 0; i < letters.length; i++) {
-    var number = letters[i].textContent;
-    console.log(number);
+    var grade = letters[i].textContent;
+    console.log(grade);
+    sum += grade;
   }
-
-  //chrome.runtime.sendMessage({myVar: gpa});
-  */
+  console.log('And the sum is = ' + sum);
+    //chrome.runtime.sendMessage({myVar: gpa});
 }
