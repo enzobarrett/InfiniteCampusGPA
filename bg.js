abaf4f5f91a8773edca40753474acaa89d2b9fe9
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-18 07:38:54 -0700

    Update bg.js

--- bg.js
+++ bg.js
@@ -3,8 +3,16 @@ chrome.runtime.onMessage.addListener(function(message) {
     if (message.myVar) {
         console.log('message received!');
         gpa = message.myVar;
-        
         console.log('gpa is = ' + gpa);
-        chrome.runtime.sendMessage({gpa: gpa});
     }
+    if (message.gpaplease) {
+        console.log('message recieved from popup');
+        chrome.runtime.sendMessage({newgpa: gpa}
+});
+chrome.runtime.sendMessage({gpaplease: "Background page please get me the gpa"}, function(response) {
+  gpa = response.newgpa;
+     console.log('response recieved');
+     console.log(gpa);
+      document.getElementsByClassName('gpa').textContent = gpa;
+    console.log('inner html set!');
 });
