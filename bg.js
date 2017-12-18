288b42e6b068e41b63750147e153b105f6090935
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-18 02:57:47 -0700

    Update bg.js

--- bg.js
+++ bg.js
@@ -2,6 +2,7 @@ var gpa;
 chrome.runtime.onMessage.addListener(function(message) {
     if (message.myVar) {
         gpa = message.myVar;
+        console.log(gpa);
         var views = chrome.extension.getViews({
         type: "popup"
         });
