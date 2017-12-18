610eb0496df0dd51a0a4aac8c4dacd4df1211191
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-18 02:50:33 -0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -17,32 +17,20 @@
   }
   
 window.onload = function() {
-  
-    var finalvar = "";
-  var arr = document.getElementsByClassName('title');
-  for (i=0; i<arr.length; i++) {
-         finalval = finalval + arr[i].value;
-  }
-  console.log(finalval);
-  /*
+
+ 
   var element = document.getElementsByClassName("bodyBorder title").innerHTML;
-  console.log(element);
   var letters = document.querySelectorAll('.inProgressGrade b');
   var sum = 0;
   var grade;
   var gpa;
   
-  if (element == 'Grades') {
   for (i = 0; i < letters.length; i++) {
     grade = toGPA(letters[i].textContent);
     sum += grade;  
   }
   gpa = sum/i;
-  console.log('Gpa is = ' + gpa)
-  } else {
-    console.log('Please Navigate to "Grades"');
-    console.log(element);
+  if (isNaN(gpa)) {console.log('Gpa is = ' + gpa)}
   //chrome.runtime.sendMessage({myVar: gpa});
-}
-*/
+
 }
