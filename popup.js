8c3c4553f238d0e0132aa89e2468eb76aefdd607
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-18 03:19:43 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
-
+var gpa;
+chrome.runtime.onMessage.addListener(function(message) {
+  if (message.gpa) {
+    console.log('message received!');
+    gpa = message.gpa;
+    console.log('gpa is = ' + gpa);
+    document.getElementsByClassName('gpa').innerHTML = gpa;
+    console.log('inner html set!');
+    }
+});
