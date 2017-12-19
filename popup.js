3565d9982b374936a71a5b48b35de91bf11a851e
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-19 16:52:09 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,7 +3,8 @@ var gpa;
 console.log('sending message...');
 chrome.runtime.sendMessage({gpaplease: "Background page please get me the gpa"}, function(response) {
   gpa = response.newgpa;
+  console.log('gpa is = ' + gpa);
      console.log('response recieved');
       //document.getElementsByClassName('gpa').textContent = gpa;
-    console.log('inner html set!');
+    //console.log('inner html set!');
 });
