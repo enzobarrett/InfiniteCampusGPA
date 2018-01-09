3b05b118664aa90c2b896da06c2c4a495cdae787
Enzo Barrett <elbarrett01@bvsd.org>
2018-01-09 12:10:09 -0700

    fix message passing

--- bg.js
+++ bg.js
@@ -1,14 +1,16 @@
 var gpa;
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (message.myVar) {
-        console.log('message received!');
-        gpa = message.myVar;
-        console.log('gpa is = ' + gpa);
-    }
-    if (message.gpaplease) {
-        console.log('message recieved from popup');
-        sendResponse({newgpa: gpa});
-    }
-                                   
+  if (request.myVar) {
+    console.log('message received!');
+    gpa = message.myVar;
+    console.log('gpa is = ' + gpa);
+  }
+    chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
 });
-
--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 console.log('popup.js running');
 var gpa;
 console.log('sending message...');
-chrome.runtime.sendMessage({gpaplease: "Background page please get me the gpa"}, function(response) {
-  gpa = response.newgpa;
-  console.log('gpa is = ' + gpa);
-     console.log('response recieved');
-      //document.getElementsByClassName('gpa').textContent = gpa;
-    //console.log('inner html set!');
+chrome.runtime.sendMessage({
+  greeting: "hello"
+}, function(response) {
+  gpa = response.farewell;
 });
+
+console.log('gpa is = ' + gpa);
+console.log('response recieved');
+//document.getElementsByClassName('gpa').textContent = gpa;
+//console.log('inner html set!');
