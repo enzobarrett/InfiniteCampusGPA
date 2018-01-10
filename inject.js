7177278714d09599f6d84a56555dce1183687cbd
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2018-01-09 19:18:17 -0700

    edit icons and minify

--- inject.js
+++ inject.js
@@ -1,39 +1,22 @@
 function toGPA(x) {
-  if (x == 'A') {
-    return 4;
-  }
-  if (x == 'B') {
-    return 3;
-  }
-  if (x == 'C') {
-    return 2;
-  }
-  if (x == 'D') {
-    return 1;
-  }
-  if (x == 'F') {
-    return 0;
-  }
-}
-
-
-
+  if (x == 'A') {return 4;}
+  if (x == 'B') {return 3;}
+  if (x == 'C') {return 2;}
+  if (x == 'D') {return 1;}
+  if (x == 'F') {return 0;}
+};
 var element = document.getElementsByClassName("bodyBorder title").innerHTML;
 var letters = document.querySelectorAll('.inProgressGrade b');
 var sum = 0;
 var grade;
 var gpa;
-
 for (i = 0; i < letters.length; i++) {
   grade = toGPA(letters[i].textContent);
-  console.log(grade);
   sum += grade;
 }
 gpa = sum / i;
 if (!isNaN(gpa)) {
-  console.log('Gpa is = ' + gpa);
   chrome.runtime.sendMessage({
     myVar: gpa
   });
-  console.log('GPA has been sent to background page');
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-var gpa;
+
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  gpa = response.farewell;
+  var gpa = response.farewell;
   document.body.innerHTML = gpa;
 });
