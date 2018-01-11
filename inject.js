0124509a2653e0513a6a1f1e66e80c69cfafceb2
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2018-01-11 16:08:18 -0700

    update inject

--- inject.js
+++ inject.js
@@ -5,15 +5,38 @@ function toGPA(x) {
   if (x == 'D') {return 1;}
   if (x == 'F') {return 0;}
 };
-var letters = document.querySelectorAll('.inProgressGrade b');
-var sum = 0;
+var gradetop;
+var r;
 var grade;
-var gpa;
-for (i = 0; i < letters.length; i++) {
-  grade = toGPA(letters[i].textContent);
-  sum += grade;
+var rect;
+var content;
+var index;
+var class = document.querySelectorAll('.gradesLink b');
+var grade = document.querySelectorAll('.inProgressGrade b');
+
+var rect = grade.getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+for (i = 0; i < grade.length; i++) {
+  var rect = grade[i].getBoundingClientRect();
+  gradetop = rect.top;
+  for (r = 1; r < class.length; r += 2) {
+    content = class[r].textContent;
+    index = content.search("wt");
+    ap = content.search("ap");
+    pib = content.search("pib");
+    if (index > -1 || ap > -1 || pib > -1) {
+      //Is a weighted class
+      sum += 1;
+      
+    }
+  }
+console.log(gradetop);
+console.log(i);
+r++;
 }
-gpa = sum / i;
+/*gpa = sum / i;
 if (!isNaN(gpa)) {
   chrome.runtime.sendMessage({myVar: gpa});
 }
+*/
